3b17fc17e1630cb360304ec257ce7d7d3dd0cf45
Caique <dev.caiquemorales@gmail.com>
2022-03-23 15:22:11 -0300

    state declared

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,46 @@
+import { useReducer } from "react";
 import "./App.css";
 
+const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  DELETE_DIGIT: "delete-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  EVALUATE: "evaluate",
+  CLEAR: "clear",
+};
+
+function reducer(state, { type, payload }) {}
+
 export function App() {
+  const [{ previousOperand, currentOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
   return (
     <>
       {/* Te amo amor */}
       <div className="calculator">
         <div className="output">
-          <div className="previous-operand">123,123</div>
-          <div className="current-operand">321,321</div>
+          <div className="previous-operand">
+            {previousOperand} {operation}
+          </div>
+          <div className="current-operand">{currentOperand}</div>
         </div>
         <div className="calculator-btn-area">
           <button className="span-2"> AC </button>
           <button> DEL </button>
           <button> ÷ </button>
+          <button> 1 </button>
+          <button> 2 </button>
+          <button> 3 </button>
+          <button> * </button>
+          <button> 4 </button>
+          <button> 5 </button>
+          <button> 6 </button>
+          <button> + </button>
           <button> 7 </button>
           <button> 8 </button>
           <button> 9 </button>
-          <button> * </button>
-          <button> 6 </button>
-          <button> 5 </button>
-          <button> 4 </button>
-          <button> + </button>
-          <button> 3 </button>
-          <button> 2 </button>
-          <button> 1 </button>
           <button> - </button>
           <button> . </button>
           <button> 0 </button>
